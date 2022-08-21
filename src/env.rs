cf2f4323d77af0caee7e3f054b46cf31ca79cadb
Weird Constructor <weirdconstructor@gmail.com>
2022-08-21 16:21:59 +0200

    Refined envelope pieces

--- src/env.rs
+++ src/env.rs
@@ -2,12 +2,15 @@
 // This file is a part of synfx-dsp. Released under GPL-3.0-or-later.
 // See README.md and COPYING for details.
 
+use crate::TRIG_LOW_THRES;
+
 struct EnvState {
     srate_ms: f32,
     stage: u32,
     phase: f32,
     inc: f32,
     start: f32,
+    current: f32,
 }
 
 impl EnvState {
@@ -18,46 +21,62 @@ impl EnvState {
             phase: 0.0,
             inc: 0.0,
             start: 0.0,
+            current: 0.0,
         }
     }
 }
 
 #[macro_export]
-macro_rules! env_delay_stage {
-    ($state: expr, $stage_idx: expr, $time_ms: expr, $out: expr) => {
-        if $state.stage == $stage_idx {
-            $state.phase = 0.0;
-            $state.stage += 1;
-            $state.start = $out;
-        } else if $state.stage == ($stage_idx + 1) {
+macro_rules! env_hold_stage {
+    ($state: expr, $stage_idx: expr, $time_ms: expr) => {
+        if $state.stage == $stage_idx || $state.stage == ($stage_idx + 1) {
+            if $state.stage == $stage_idx {
+                $state.phase = 0.0;
+                $state.stage += 1;
+                $state.start = $state.current;
+            }
+
             let inc = 1.0 / ($time_ms * $state.srate_ms);
             $state.phase += inc;
-            if ($state.phase + inc) >= 1.0 {
+            if $state.phase >= 1.0 {
                 $state.stage += 1;
             }
-            $out = $state.start;
+            $state.current = $state.start;
         }
     };
 }
 
 #[macro_export]
 macro_rules! env_target_stage {
-    ($state: expr, $stage_idx: expr, $time_ms: expr, $value: expr, $out: expr) => {
-        if $state.stage == $stage_idx {
-            let time_samples = $time_ms * $state.srate_ms;
-            // *2.0 to make it stop one step earlier, to really reach the target value:
-            $state.phase = 0.0;
-            $state.start = $out;
-            $state.stage += 1;
-        } else if $state.stage == ($stage_idx + 1) {
+    ($state: expr, $stage_idx: expr, $time_ms: expr, $value: expr, $shape_fn: expr) => {
+        if $state.stage == $stage_idx || $state.stage == ($stage_idx + 1) {
+            if $state.stage == $stage_idx {
+                $state.phase = 0.0;
+                $state.start = $state.current;
+                $state.stage += 1;
+            }
+
             let inc = 1.0 / ($time_ms * $state.srate_ms);
             $state.phase += inc;
-            if ($state.phase + inc) >= 1.0 {
+            if $state.phase >= 1.0 {
                 $state.stage += 1;
-                $out = $value;
+                $state.current = $value;
             } else {
-                $out = $state.start * (1.0 - $state.phase) + $state.phase * $value;
+                $state.current = ($shape_fn)($state.start * (1.0 - $state.phase) + $state.phase * $value);
             }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! env_sustain_stage {
+    ($state: expr, $stage_idx: expr, $sustain_value: expr, $gate: expr) => {
+        if $state.stage == $stage_idx {
+            if $gate < TRIG_LOW_THRES {
+                $state.stage += 1;
+            }
+
+            $state.current = $sustain_value;
         }
     };
 }
@@ -67,36 +86,90 @@ mod test {
     use super::*;
 
     #[test]
-    fn check_delay_stage() {
+    fn check_hold_stage() {
         let mut state = EnvState::new();
 
-        let mut v = 0.6;
+        state.current = 0.6;
         for _ in 0..88 {
-            env_delay_stage!(state, 0, 2.0, v);
-            println!("V[{:6.4}]: {:6.4}", state.phase, v);
+            env_hold_stage!(state, 0, 2.0);
+            println!("V[{:6.4} / {}]: {:6.4}", state.phase, state.stage, state.current);
             assert!(state.stage == 1);
-            assert!(v > 0.5);
+            assert!(state.current > 0.5);
         }
 
-        env_delay_stage!(state, 0, 2.0, v);
+        env_hold_stage!(state, 0, 2.0);
         assert!(state.stage == 2);
-        assert!(v > 0.5);
+        assert!(state.current > 0.5);
     }
 
     #[test]
     fn check_target_stage() {
         let mut state = EnvState::new();
 
-        let mut v = 0.0;
         for _ in 0..88 {
-            env_target_stage!(state, 0, 2.0, 0.6, v);
+            env_target_stage!(state, 0, 2.0, 0.6, |x| x);
             assert!(state.stage == 1);
-            println!("V[{:6.4} / {}]: {:6.4}", state.phase, state.stage, v);
+            println!("V[{:6.4} / {}]: {:6.4}", state.phase, state.stage, state.current);
         }
 
-        env_target_stage!(state, 0, 2.0, 0.6, v);
+        env_target_stage!(state, 0, 2.0, 0.6, |x| x);
         assert!(state.stage == 2);
-        println!("V[{:6.4} / {}]: {:6.4}", state.phase, state.stage, v);
-        assert!(v >= 0.5999);
+        println!("V[{:6.4} / {}]: {:6.4}", state.phase, state.stage, state.current);
+        assert!(state.current >= 0.5999);
+    }
+
+    #[test]
+    fn check_very_short_target_stage() {
+        let mut state = EnvState::new();
+
+        env_target_stage!(state, 0, 0.01, 0.6, |x| x);
+        assert!(state.stage == 2);
+        assert!(state.current == 0.6);
+        println!("V[{:6.4} / {}]: {:6.4}", state.phase, state.stage, state.current);
+    }
+
+    #[test]
+    fn check_short_target_stage() {
+        let mut state = EnvState::new();
+
+        env_target_stage!(state, 0, 0.03, 0.6, |x| x);
+        println!("V[{:6.4} / {}]: {:6.4}", state.phase, state.stage, state.current);
+        assert!(state.stage == 1);
+        assert!((state.current - 0.4535).abs() < 0.0001);
+
+        env_target_stage!(state, 0, 0.03, 0.6, |x| x);
+        println!("V[{:6.4} / {}]: {:6.4}", state.phase, state.stage, state.current);
+        assert!(state.stage == 2);
+        assert!(state.current == 0.6);
+    }
+
+    #[test]
+    fn check_sustain_stage() {
+        let mut state = EnvState::new();
+
+        env_sustain_stage!(state, 0, 0.5, 1.0);
+        println!("V[{:6.4} / {}]: {:6.4}", state.phase, state.stage, state.current);
+        assert!(state.stage == 0);
+        assert!((state.current - 0.5).abs() < 0.0001);
+
+        env_sustain_stage!(state, 0, 0.5, 1.0);
+        println!("V[{:6.4} / {}]: {:6.4}", state.phase, state.stage, state.current);
+        assert!(state.stage == 0);
+        assert!((state.current - 0.5).abs() < 0.0001);
+
+        env_sustain_stage!(state, 0, 0.5, 0.0);
+        println!("V[{:6.4} / {}]: {:6.4}", state.phase, state.stage, state.current);
+        assert!(state.stage == 1);
+        assert!((state.current - 0.5).abs() < 0.0001);
+    }
+
+    #[test]
+    fn check_sustain_stage_short() {
+        let mut state = EnvState::new();
+
+        env_sustain_stage!(state, 0, 0.5, 0.0);
+        println!("V[{:6.4} / {}]: {:6.4}", state.phase, state.stage, state.current);
+        assert!(state.stage == 1);
+        assert!((state.current - 0.5).abs() < 0.0001);
     }
 }
